9ea65885ef3c86ed1df5cf3d9c3c5c9898eaf6fc
Anniek <anniekgithub@gmail.com>
2023-05-30 18:08:56 +0200

    feature delete user

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { fetchUsers, addUser } from "../store";
+import { fetchUsers, addUser, removeUser } from "../store";
 import Button from "./Button";
 import SkeletonLoader from "./SkeletonLoader";
 import { useThunk } from "../hooks/use-thunk";
+import UserListItem from "./UserListItem";
 
 function UsersList() {
     //keep track loading big list of users
@@ -31,13 +32,7 @@ function UsersList() {
         content = <div>Error fetching data....</div>;
     } else {
         content = data.map((user) => {
-            return (
-                <div key={user.id} className="mb-2 border rounded">
-                    <div className="flex p-2 justify-between items-center cursor-pointer">
-                        {user.name}
-                    </div>
-                </div>
-            );
+            return <UserListItem key={user.id} user={user} />;
         });
     }
 
