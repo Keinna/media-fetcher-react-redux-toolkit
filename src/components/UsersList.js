03076a9a9f62390c2f07fd0d6119200e96348a2d
Anniek <anniekgithub@gmail.com>
2023-05-30 15:42:20 +0200

    refactor and change skeleton display

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -24,26 +24,26 @@ function UsersList() {
         doCreateUser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <SkeletonLoader times={6} className="h-10 w-full" />;
-    }
-    if (loadingUsersError) {
-        return <div>Error fetching data....</div>;
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
+        content = <SkeletonLoader times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data....</div>;
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center cursor-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        );
-    });
+            );
+        });
+    }
 
     return (
         <div>
-            <div className="flex flex-row justify-between m-3">
+            <div className="flex flex-row justify-between items-center m-3">
                 <h1 className="m-2 text-xl">Users </h1>
                 <Button loading={isCreatingUser} onClick={handleUserAdd}>
                     + Add User
@@ -51,7 +51,7 @@ function UsersList() {
                 {creatingUsersError &&
                     "Error creating user..." + creatingUsersError.err}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 }
